ea90604e5c7ae7fad3a69a55a6d0c892cf54ce44
nchenche <nicolas.chevrollier@laposte.net>
2020-11-28 20:36:42 +0100

    Add premice for iterhmmbuilder on directory

--- iterhmmbuild/main.py
+++ iterhmmbuild/main.py
@@ -4,7 +4,8 @@
 Created on Mon Jul 16 15:45:57 2018
 
 """
-# import os
+import os
+import glob
 import sys
 
 import lib.logHandler as logHandler
@@ -16,9 +17,20 @@ def main():
     logger = logHandler.Logger(name='main', outpath=param.outdirname)
     param.description()
 
-    iterhmmbuilder = pipeline.IterHmmBuilder(_input=param.fasta_fname, input_db=param.protdb, param=param)
-    logger.title("Running iterHmmBuilder")
-    iterhmmbuilder.run()
+    if os.path.isdir(param.fasta_fname):
+        fasta_filenames = (x for x in glob.glob(param.fasta_fname + '/' + '*.fa'))
+        if fasta_filenames:
+            for fasta_filename in fasta_filenames:
+                param.hmm_name = os.path.basename(fasta_filename).split('.')[0]
+                iterhmmbuilder = pipeline.IterHmmBuilder(_input=fasta_filename, input_db=param.protdb, param=param)
+                logger.title("Running iterHmmBuilder")
+                iterhmmbuilder.run()
+
+    else:
+        iterhmmbuilder = pipeline.IterHmmBuilder(_input=param.fasta_fname, input_db=param.protdb, param=param)
+        logger.title("Running iterHmmBuilder")
+        iterhmmbuilder.run()
+
 
 if __name__ == '__main__':
     sys.exit(main())
