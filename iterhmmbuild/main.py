73db46c5dcaa5588354c57c82acace98730065b8
nchenche <nicolas.chevrollier@laposte.net>
2020-11-28 19:59:40 +0100

    Change logger info from main to pipeline

--- iterhmmbuild/main.py
+++ iterhmmbuild/main.py
@@ -8,8 +8,7 @@ Created on Mon Jul 16 15:45:57 2018
 import sys
 
 import lib.logHandler as logHandler
-import iterhmmbuild.lib.parameters as parameters
-import iterhmmbuild.lib.pipeline as pipeline
+from iterhmmbuild.lib import parameters, pipeline
 
 
 def main():
@@ -17,18 +16,9 @@ def main():
     logger = logHandler.Logger(name='main', outpath=param.outdirname)
     param.description()
 
-    sys.exit()
-
     iterhmmbuilder = pipeline.IterHmmBuilder(_input=param.fasta_fname, input_db=param.protdb, param=param)
     logger.title("Running iterHmmBuilder")
     iterhmmbuilder.run()
-
-    logger.title('Output files for {} HMM profile'.format(param.hmm_name))
-    logger.info('HMM profile file:   \t{}'.format(iterhmmbuilder.output_hmm))
-    logger.info('Fasta seed file:    \t{}'.format(iterhmmbuilder.output_fasta))
-    logger.info('Alignment seed file:\t{}'.format(iterhmmbuilder.output_muscle))
-    logger.info('')
-
 
 if __name__ == '__main__':
     sys.exit(main())
