103793d764089349549070cb750c8a312a0a536e
nchenche <nicolas.chevrollier@laposte.net>
2020-11-29 13:33:59 +0100

    Update run iterhmmbuilder on directory input.
    Now, a fasta files containing directory can be used as input for iterhmmbuilder. The script will keep its output architecture and adds a final database.hmm file.

--- iterhmmbuild/main.py
+++ iterhmmbuild/main.py
@@ -1,12 +1,7 @@
-#!/usr/bin/python2.7
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 16 15:45:57 2018
-
-"""
 import os
 import glob
 import sys
+from shutil import copyfile, rmtree
 
 import lib.logHandler as logHandler
 from iterhmmbuild.lib import parameters, pipeline
@@ -20,16 +15,31 @@ def main():
     if os.path.isdir(param.fasta_fname):
         fasta_filenames = (x for x in glob.glob(param.fasta_fname + '/' + '*.fa'))
         if fasta_filenames:
-            for fasta_filename in fasta_filenames:
-                param.hmm_name = os.path.basename(fasta_filename).split('.')[0]
-                iterhmmbuilder = pipeline.IterHmmBuilder(_input=fasta_filename, input_db=param.protdb, param=param)
-                logger.title("Running iterHmmBuilder")
-                iterhmmbuilder.run()
-
+            run_iter_ondir(fasta_filenames=fasta_filenames, param=param, logger=logger)
     else:
         iterhmmbuilder = pipeline.IterHmmBuilder(_input=param.fasta_fname, input_db=param.protdb, param=param)
         logger.title("Running iterHmmBuilder")
         iterhmmbuilder.run()
+
+
+def run_iter_ondir(fasta_filenames, param, logger):
+    hmm_outputs = []
+    outdirname_base = param.outdirname
+    for fasta_filename in fasta_filenames:
+        param.hmm_name = os.path.basename(fasta_filename).split('.')[0]
+        param.outdirname = outdirname_base + param.hmm_name + '/'
+
+        iterhmmbuilder = pipeline.IterHmmBuilder(_input=fasta_filename, input_db=param.protdb, param=param)
+        logger.title("Running iterHmmBuilder")
+        iterhmmbuilder.run()
+        hmm_outputs.append(iterhmmbuilder.output_hmm)
+
+    hmmdir = outdirname_base + 'hmmdir/'
+    os.makedirs(hmmdir, exist_ok=True)
+    [copyfile(src, hmmdir + os.path.basename(src)) for src in hmm_outputs]
+    cmd = 'create_hmmdb ' + '-hmmdir ' + outdirname_base + 'hmmdir/' + ' -outdir ' + outdirname_base
+    os.system(cmd)
+    rmtree(hmmdir)
 
 
 if __name__ == '__main__':
